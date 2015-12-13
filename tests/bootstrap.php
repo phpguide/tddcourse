e5104631f497564d949fe39a3527b11427b962e8
intval <Alex@phpguide.co.il>
2015-12-13 20:56:54 -0200

    commit

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @var $autoloader Composer\Autoload\ClassLoader
+ */
+$autoloader = require __DIR__.'/../vendor/autoload.php';
+
+$autoloader->addPsr4('', __DIR__.'/../src');
+$autoloader->addPsr4('', __DIR__.'/../tests');
+
